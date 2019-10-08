f18b8d028fe50659eb9b9f09cce4301503d09981
IsaacAlonzo <isaac.alonzo@icloud.com>
2019-10-08 15:27:02 -0500

    example of """ command use

--- Weekof10-7.py
+++ Weekof10-7.py
@@ -63,6 +63,20 @@ print(formatter % (formatter, formatter, formatter, formatter))
 
 # why did I use%r instead of %s in the above example?
 
-# which should I use on a regualr basis
+# which should I use on a regular basis
+# using %s would be fine but if you wanted to use %r if you want
 
 # why does %r sometimes give me a single quotes around things?
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("here are the days: ", days)
+print("here are the months: ", months)
+
+print("""
+There's something going on here.
+With three double quotes.
+We"ll be able to type as much as we like.
+Even 4 lines if we want. or 10, ir 100
+""")
